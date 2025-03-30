6b775fdd78b4c8969607770f7a103505f12632ba
GUGUKANG <GUGUKANG114514@gmail.com>
2025-03-30 16:31:55 +0800

    更改vite.config.ts，不删除user.ini

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import fs from 'fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'preserve-user-ini',
+      enforce: 'post',
+      closeBundle() {
+        const filePath = 'dist/.user.ini'
+        if (!fs.existsSync(filePath)) {
+          fs.writeFileSync(filePath, '') // 确保 .user.ini 存在
+        }
+      }
+    }
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
